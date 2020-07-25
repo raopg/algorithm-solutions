b83252fc7a7e71ac3ebe0404a10a2de3f7832152
raopg <prateek.rao.08@gmail.com>
2020-07-25 10:52:06 -0700

    remove dups follow-up no space

--- CTCI/Linked_Lists/removeDups.py
+++ CTCI/Linked_Lists/removeDups.py
@@ -1,6 +1,7 @@
 ## Problem: Given a linked list, remove all the duplicates.
 
 ## Solution: We can simply maintain a HashSet of the visited nodes and remove a node if its data is ever repeated.
+## Time and Space Complexity: O(N) and O(N)
 
 class ListNode:
     def __init__(self, val = 0, next = None):
@@ -25,5 +26,21 @@ def removeDups(head: ListNode) -> ListNode:
             del toDelete
         else:
             seen.add(temp.next.val)
+    
+    return head
+
+## Follow-up: Remove duplicates no space -> we have to sacrifice time.
+
+def removeDuplicateNoSpace(head: ListNode) -> ListNode:
+    temp = head
+    
+    while temp != None:
+        lookahead = temp
+        while lookahead.next != None:
+            if lookahead.next.val == temp.val:
+                lookahead.next = lookahead.next.next
+            else:
+                lookahead = lookahead.next
+        temp = temp.next
     
     return head
