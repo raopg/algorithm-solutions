173f2c1c311d409ec9328aa296c18b1d257082d0
raopg <prateek.rao.08@gmail.com>
2020-07-27 20:54:06 -0700

    Is symmetric tree

---
+++ Python/binaryTreeSymmetric.py
@@ -0,0 +1,19 @@
+###################
+## Find out if a binary tree is symmetric or not
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.isMirror(root, root)
+    
+    def isMirror(self, node1, node2):
+        if not node1 and not node2:
+            return True
+        if not node1 or not node2:
+            return False
+        return node1.val == node2.val and self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
