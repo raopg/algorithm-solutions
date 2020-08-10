40e0a74301d8884a9ee31a97d961b6b5227da103
raopg <prateek.rao.08@gmail.com>
2020-08-10 06:47:08 -0700

    fixed succsor

--- CTCI/Trees_and_Graphs/successor.py
+++ CTCI/Trees_and_Graphs/successor.py
@@ -1,5 +1,10 @@
-## Problem: Given access to a single node in a BST, find its successor.
+## Problem: Given access to a single node in a BST, find its in-order successor.
 
+## In-order goes => left_subtree -> root -> right_subtree
+## There are a few cases here:
+## 1: a node with a right subtree(right node). In this case, we want to return the leftmost child
+## 2: a node with no right subtree. In this case, we are in the left subtree (possibly), 
+##  and we need to get back to the parent.
 class TreeNode:
     def __init__(self, val = 0):
         self.val = val
@@ -10,11 +15,22 @@ class TreeNode:
 
 def successor(node):
     if node is None:
-        return
+        return None
+    else:
+        q = node
+        x = q.parent
+        while x and x.left != q:
+            q = x
+            x = x.parent
+        return x
+
+def leftMostChild(node):
+    if node is None:
+        return None
     
-    if node.right:
-        return node.right
+    while node.left:
+        node = node.left
     
-    return successor(node.parent)
+    return node
 
 
