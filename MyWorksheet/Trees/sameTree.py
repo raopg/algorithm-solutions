69d98929e01fc01872fe9baabc8dfd3f0afcddb8
raopg <prateek.rao.08@gmail.com>
2020-08-14 10:41:25 -0700

    Same tree

---
+++ MyWorksheet/Trees/sameTree.py
@@ -0,0 +1,11 @@
+## Problem: Given two trees, check whether they are the same.
+## Solution: Recurse thru tree. O(N) time, constant space.
+
+
+def sameTree(t1, t2):
+    if t1 is None and t2 is None:
+        return True
+    if t1 is None or t2 is None:
+        return False
+    return t1 is t2 and sameTree(t1.left, t2.left) and sameTree(t1.right, t2.right)
+
