f83a5a2e041b79b1c4b76c21aa18382b18ac046f
raopg <prateek.rao.08@gmail.com>
2020-08-27 19:06:43 -0700

    Refactored suboptimal garbage

--- Python/first_and_last_sorted_array.py
+++ Python/first_and_last_sorted_array.py
@@ -1,6 +1,6 @@
 ## Problem: Given a sorted array and target value, find the first and last occurence(index) of the element
 
-## Solution: Binary search. Once you find the target, loop backwards and forwards to find the
+## Solution1 : Binary search. Once you find the target, loop backwards and forwards to find the
 ## first and last occurence. Return that pair of indices.
 
 def find_first_last(arr, key):
@@ -26,6 +26,47 @@ def find_first_last(arr, key):
     
     return (-1,-1)
 
+## The above solution is suboptimal because it can devolve into an O(n) algorithm (Ex: If the entire array)
+## consisted of only the target.
+
+## Here is a optimized approach:
+## We use binary search for two purposes, and we can denote which purpose using a boolean flag.
+## If the flag is true, we find the leftmost occurrence of the key using binary search
+## Else, we find the rightmost occurence of the key using binary search.
+## In our main function, we run this search once to find the leftmost occurence. If we cant find
+## anything, we immediately return -1,-1
+## Else, we return the left bound, and the right bound (which we get after calling binary search with flag = False)
+
+def binary_search_extreme(arr, target, left):
+    bound = None
+    l = 0
+    r = len(arr) - 1
+
+    while l <= r:
+        mid = l + (r - l) // 2
+
+        if left:
+            if arr[mid] >= target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        else:
+            if arr[mid] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        if arr[mid] == target:
+            bound = mid
+    
+    return bound if bound else -1
+
+def search(arr, target):
+    left_bound = binary_search_extreme(arr, target, True)
+
+    if left_bound == -1:
+        return [-1, -1]
+    
+    return [left_bound, binary_search_extreme(arr, target, False)]
 
 if __name__ == "__main__":
     arr = [2,3,6,7,7,8,8,8,12]
@@ -33,5 +74,9 @@ if __name__ == "__main__":
     for i, val in enumerate(arr):
         print(i,':', val, end = "  ")
 
-    print(find_first_last(arr, 8))
-    print(find_first_last(arr, 7))
+    # print(find_first_last(arr, 8))
+    # print(find_first_last(arr, 7))
+
+    print()
+    print(search(arr, 8))
+    print(search(arr, 7))
